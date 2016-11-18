d74655c493a6c3fe8d6b7caed2fe4e3aba037b80
Colin Hill <colin@peak2peakmedia.com>
2016-11-18 15:03:09 +0000

    [Add] Webpack, and implemented Bootstrap

---
+++ webpack/js/boot.js
@@ -0,0 +1,9 @@
+import {bootstrap} from 'angular2/platform/browser'
+import {TodoComponent} from './TodoComponent'
+
+let boot = document.addEventListener('DOMContentLoaded', () => {
+  bootstrap(TodoComponent)
+})
+
+
+module.exports = boot
